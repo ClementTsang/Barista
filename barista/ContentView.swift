09a46bf500ea08f560940009a530cb5928404186
ClementTsang <clement.tsang@datadoghq.com>
2023-11-20 23:08:56 -0500

    init

---
+++ barista/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  barista
+//
+//  Created by Clement Tsang on 2023-11-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ barista/baristaApp.swift
@@ -0,0 +1,17 @@
+//
+//  baristaApp.swift
+//  barista
+//
+//  Created by Clement Tsang on 2023-11-20.
+//
+
+import SwiftUI
+
+@main
+struct baristaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
